3c8f0dd9686dee7dc03a3fd6929db30bb9aa9586
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-04-23 07:04:27 -0700

    create login and singup component and style

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import "./Login.css";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      <h1>This is login page</h1>
+    <div className="form-container">
+      <h1 className="form-title">Log In</h1>
+      <form>
+        <div className="form-control">
+          <label htmlFor="">Email</label>
+          <input type="email" name="email" id="email" required />
+        </div>
+        <div className="form-control">
+          <label htmlFor="">Password</label>
+          <input type="password" name="password" id="password" required />
+        </div>
+        <input className="btn-submit" type="submit" value="Login" />
+      </form>
+      <p>
+        <small>
+          New to Ema-John? <Link to="/singup">Create new account</Link>
+        </small>
+      </p>
     </div>
   );
 };
--- src/components/SingUp/SingUp.jsx
+++ src/components/SingUp/SingUp.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import "./SingUp.css";
+import { Link } from "react-router-dom";
 
 const SingUp = () => {
   return (
-    <div>
-      <h1>SingUp</h1>
+    <div className="form-container">
+      <h1 className="form-title">Sing Up</h1>
+      <form>
+        <div className="form-control">
+          <label htmlFor="">Email</label>
+          <input type="email" name="email" id="email" required />
+        </div>
+        <div className="form-control">
+          <label htmlFor="">Password</label>
+          <input type="password" name="password" id="password" required />
+        </div>
+        <div className="form-control">
+          <label htmlFor="">Confirm Password</label>
+          <input type="password" name="confirm" id="password" required />
+        </div>
+        <input className="btn-submit" type="submit" value="Sing Up" />
+      </form>
+      <p>
+        <small>
+          Already have an account? <Link to="/login">Log In</Link>
+        </small>
+      </p>
     </div>
   );
 };
