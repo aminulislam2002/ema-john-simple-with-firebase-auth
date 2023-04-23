7edfb1325433147bc4f9f5ed59bdc253c88044f0
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-04-23 08:24:39 -0700

    setup auth context under usercontext to share auth info

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Header.css";
 import logo from "../../assets/images/Logo.svg";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProvider";
 
 const Header = () => {
+  const { user } = useContext(AuthContext);
   return (
     <nav className="header">
       <img src={logo} alt="" />
@@ -14,6 +16,7 @@ const Header = () => {
         <Link to="/login">Login</Link>
         <Link to="/singup">Sing Up</Link>
       </div>
+      {user && <span>Welcome {user.displayName}</span>}
     </nav>
   );
 };
--- src/components/SingUp/SingUp.jsx
+++ src/components/SingUp/SingUp.jsx
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SingUp.css";
 import { Link } from "react-router-dom";
 
 const SingUp = () => {
+  const [error, setError] = useState(" ");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const confirm = form.confirm.value;
+    console.log(email, password, confirm);
+
+    if (password !== confirm) {
+      setError("Your password did not match");
+      return;
+    } else if (password.length < 6) {
+      setError("password must be 6 characters or longer");
+      return;
+    }
+  };
+
   return (
     <div className="form-container">
       <h1 className="form-title">Sing Up</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-control">
-          <label htmlFor="">Email</label>
+          <label htmlFor="email">Email</label>
           <input type="email" name="email" id="email" required />
         </div>
         <div className="form-control">
-          <label htmlFor="">Password</label>
+          <label htmlFor="password">Password</label>
           <input type="password" name="password" id="password" required />
         </div>
         <div className="form-control">
-          <label htmlFor="">Confirm Password</label>
-          <input type="password" name="confirm" id="password" required />
+          <label htmlFor="confirm">Confirm Password</label>
+          <input type="password" name="confirm" id="confirm" required />
         </div>
         <input className="btn-submit" type="submit" value="Sing Up" />
       </form>
@@ -26,6 +46,7 @@ const SingUp = () => {
           Already have an account? <Link to="/login">Log In</Link>
         </small>
       </p>
+      <p className="text-error">{error}</p>
     </div>
   );
 };
